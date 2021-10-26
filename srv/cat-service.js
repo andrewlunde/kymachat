8781313602d7ab624438eedeadc5a4dc242292f0
Andrew Lunde <andrew.lunde@sap.com>
2021-10-26 16:49:35 -0400

    pre custom types

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -8,25 +8,28 @@ const util = require('util');
  */
 module.exports = cds.service.impl(async function() {
  
-    this.on('READ', 'Books', async (req, next) => {
-        console.log("ATTACH before-spawn");
-        cds.spawn({}, async () => {
-            // ATTACH statement seems to have no effect in this context.
-            await cds.db.run('ATTACH DATABASE \'db/sub01.db\' AS SUB');
-            const schema = await cds.db.run('SELECT * FROM sqlite_master');
-            console.log("SCHEMA: " + util.inspect(schema,false,3));
-            console.log("ATTACH after-spawn");
-            return next();
-        });
-    });
-    
+    // this.on('READ', 'Books', async (req, next) => {
+    //     console.log("ATTACH before-spawn");
+    //     cds.spawn({}, async () => {
+    //         // ATTACH statement seems to have no effect in this context.
+    //         // await cds.db.run('ATTACH DATABASE \'db/sub01.db\' AS SUB');
+    //         const tx = cds.tx(req);
+    //         await tx.run('ATTACH DATABASE \'db/sub01.db\' AS SUB');
+    //         const schema = await cds.db.run('SELECT * FROM sqlite_master');
+    //         console.log("SCHEMA: " + util.inspect(schema,false,3));
+    //         console.log("ATTACH after-spawn");
+    //         return next();
+    //     });
+    // });
+
+
     // this.before ('*','Books', async (req) => {
     //     // console.log("req: " + util.inspect(req,false,2));
     //     console.log("before Books: ");
 
     //     const db = await cds.connect.to('db');
     //     await db.tx();
-    //     await db.run('ATTACH DATABASE "db/sub02.db" AS SUB');
+    //     await db.run('ATTACH DATABASE \'db/sub01.db\' AS SUB');
     //     await db.commit();
 
     //     console.log("ATTACH");
@@ -49,17 +52,25 @@ module.exports = cds.service.impl(async function() {
 	//     }
     // });
 
-    // this.after('READ', 'Books', booksData => {
-    //     const books = Array.isArray(booksData) ? booksData : [booksData];
-    //     books.forEach(book => {
-    //         if (book.stock >= 100) {
-    //             book.code = 1;
-    //         } else {
-    //             book.code = 2;
-    //         }
-    //     });
-    //   	console.log("after READ Books: ");
+    this.before('READ', 'Books', req => {
+        req.query.where("SubID = '0x1234'");
+    });
+
+    // this.before('CREATE', 'Books', req => {
+    //     req.query.where("SubID = '0x1234'");
     // });
+
+    this.after('READ', 'Books', booksData => {
+        const books = Array.isArray(booksData) ? booksData : [booksData];
+        books.forEach(book => {
+            if (book.stock >= 100) {
+                book.code = 1;
+            } else {
+                book.code = 2;
+            }
+        });
+      	console.log("after READ Books: ");
+    });
 
 });
 
